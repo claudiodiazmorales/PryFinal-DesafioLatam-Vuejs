a3cfe391eb259b3c9dda1f85d92e7c76c7c03550
Claudio Díaz <diazmclaudio.dev@gmail.com>
2021-06-08 19:08:44 -0400

    proyecto final desafio latam - VueJS

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <Home />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Home from '@/views/Home.vue';
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Home
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Inicio />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Inicio from '@/components/Inicio.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Inicio
+  }
+}
+</script>
